eb1c748e7b446dedae2a5fb78a79dd53a22d4ee7
kapilsabharwal <kapilsabharwal@gmail.com>
2026-03-10 20:13:35 +0000

    First commit to learn about version controlling

---
+++ mycode-backup/custom_functions/broken_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Ghostbusters Lab | Step 2"""
+
+def report_ghost_sighting():
+    """Prints a message about a ghost sighting."""
+    print("Ghost sighted! Who you gonna call?")
+
+# Function calls
+report_ghost_sighting()
+report_ghost_sighting()
+report_ghost_sighting()
